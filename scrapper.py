c8e7956040ae7bc7ee8bd1b1b63378ff2a5f37ba
Pedro Matos <ma2s.pedro@gmail.com>
2022-10-30 10:21:42 +0100

    parsing the chf offers with -

--- scrapper.py
+++ scrapper.py
@@ -9,14 +9,15 @@ headers = {
     "USer-Agent": "Chrome/107.0.5304.68"
 }
 
-response = requests.get("https://www.digitec.ch/en/s1/product/noctua-nh-d15-chromax-165-mm-cpu-coolers-12111628", headers)
+response = requests.get("https://www.digitec.ch/en/s1/product/apple-iphone-14-pro-256-gb-space-black-610-sim-esim-48-mpx-5g-mobile-phones-21997003", headers)
 soup = BeautifulSoup(response.content, 'html.parser')
 
 def get_product() -> str: 
     return soup.h1.strong.get_text() + soup.h1.span.get_text()
 
 def parse_price(raw_price: str) -> Union[float, None]:
-    return float(price_pattern.group(0)) if (price_pattern := re.search(r"\d+\.(\d+|.)", raw_price)) else None
+    return float(price_pattern_digits.group(0)) if (price_pattern_digits := re.search(r"\d+\.\d+", raw_price)) else \
+    float(price_pattern_w_chars.group(0).split(".")[0]) if (price_pattern_w_chars := re.search(r"\d+\..", raw_price)) else None
 
 def get_offers() -> List[float]:
     offers = []
@@ -25,4 +26,4 @@ def get_offers() -> List[float]:
             offers.append(offer)
     return offers
 
-print(f"Product: {get_product()}\nPrice: {get_offers()}")
+print(f"Product: {get_product()}\nOffers: {get_offers()}")
