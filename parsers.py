dedcb6bbfc7ded59cce2448ba0f76f0286151003
Pedro Matos <ma2s.pedro@gmail.com>
2022-11-10 22:36:55 +0100

    abstract class and digitec subclass

--- parsers.py
+++ parsers.py
@@ -1,7 +1,15 @@
+import re
+import requests
+
 from abc import ABC, abstractmethod
 from bs4 import BeautifulSoup
 from offer import Offer
-from typing import List
+from typing import List, Union
+
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/41.0.2228.0 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'
+           }
+
 
 class Parser(ABC):
     links: list[str]
@@ -9,7 +17,34 @@ class Parser(ABC):
 
     def __init__(self, links: List[str]):
         self.links = links
-        
+
     @abstractmethod
-    def get_offers(self) -> list[Offer]:
+    def get_product(self, soup: BeautifulSoup) -> str:
         pass
+
+    @abstractmethod
+    def get_price(self, soup: BeautifulSoup) -> Union[float, None]:
+        pass
+
+    def get_offers(self) -> list[Offer]:
+        offers = []
+        for l in self.links:
+            response = requests.get(l, headers=HEADERS)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            if price := self.get_price(soup):
+                offers.append(Offer(link=l, product=self.get_product(soup), price=price))
+        return offers
+
+
+class DigitecParser(Parser):
+    def get_product(self, soup: BeautifulSoup) -> str:
+        pass
+
+    @staticmethod
+    def parse_price(raw_price: str) -> Union[float, None]:
+        return float(price_pattern_digits.group(0)) if (price_pattern_digits := re.search(r"\d+\.\d+", raw_price)) else\
+            float(price_pattern_w_chars.group(0).split(".")[0]) if (
+                price_pattern_w_chars := re.search(r"\d+\..", raw_price)) else None
+
+    def get_price(self, soup: BeautifulSoup) -> Union[float, None]:
+        return self.parse_price(soup.find_all('strong')[0].get_text())
--- scrapper.py
+++ scrapper.py
@@ -5,19 +5,22 @@ from bs4 import BeautifulSoup
 from discord import SyncWebhook
 from typing import List, Union
 
-webhook = SyncWebhook.from_url("https://discord.com/api/webhooks/1036384939878387723/pYZwx78HUlMxUv_6Br2kEsp69hQrxzzDqYz4UnWLn5qHs4hOptoF6IHLOswSqIKZ1h4T")
+webhook = SyncWebhook.from_url(
+    "https://discord.com/api/"
+    "webhooks/1036384939878387723/pYZwx78HUlMxUv_6Br2kEsp69hQrxzzDqYz4UnWLn5qHs4hOptoF6IHLOswSqIKZ1h4T"
+)
 
-headers = {
-    "USer-Agent": "Chrome/107.0.5304.68"
-}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/41.0.2228.0 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'
+           }
 websites = {
-    "Digitec" : [
+    "Digitec": [
         "https://www.digitec.ch/en/s1/product/xiaomi-mi-selfie-stick-tripod-mobile-phone-accessories-9868799"
-        ],
+    ],
     "Amazon": [
         "https://www.amazon.es/Profesional-Tupwoon-Resistente-Desmontable-Compatible/dp/B0B6BF9HT5",
         "https://www.amazon.es/Tr%C3%ADpode-Extensible-Inal%C3%A1mbrico-Control-Compatible/dp/B09KG9SMBV/"
-        ]
+    ]
 }
 
 
@@ -25,12 +28,13 @@ class DigitecParser:
     def __init__(self, links: List[str]):
         self.links = links
 
-    def get_product(self, soup) -> str: 
+    def get_product(self, soup) -> str:
         return soup.h1.strong.get_text() + soup.h1.span.get_text()
 
     def parse_price(self, raw_price: str) -> Union[float, None]:
         return float(price_pattern_digits.group(0)) if (price_pattern_digits := re.search(r"\d+\.\d+", raw_price)) else \
-        float(price_pattern_w_chars.group(0).split(".")[0]) if (price_pattern_w_chars := re.search(r"\d+\..", raw_price)) else None
+            float(price_pattern_w_chars.group(0).split(".")[0]) if (
+                price_pattern_w_chars := re.search(r"\d+\..", raw_price)) else None
 
     def get_price(self, soup) -> float:
         return self.parse_price(soup.find_all('strong')[0].get_text())
@@ -39,32 +43,33 @@ class DigitecParser:
         for l in self.links:
             response = requests.get(l, headers)
             soup = BeautifulSoup(response.content, 'html.parser')
-            #print(f"Product: {self.get_product(soup)}\nPrice: {self.get_price(soup)}")
-            webhook.send(f"Digitec:\n\t\t{self.get_product(soup)}\n\t\t - Link: {l}\n\t\t- Price: {self.get_price(soup)}")
+            # print(f"Product: {self.get_product(soup)}\nPrice: {self.get_price(soup)}")
+            webhook.send(
+                f"Digitec:\n\t\t{self.get_product(soup)}\n\t\t - Link: {l}\n\t\t- Price: {self.get_price(soup)}")
 
 
-        
 class AmazonParser:
     def __init__(self, links: List[str]):
         self.links = links
 
-    def get_product(self, soup) -> str: 
+    def get_product(self, soup) -> str:
         return soup.find(id="productTitle").get_text().strip()
 
     def parse_price(self, raw_price: str) -> Union[float, None]:
         return float(raw_price)
 
-
     def get_price(self, soup) -> float:
-        return float(soup.find(id='corePriceDisplay_desktop_feature_div').select('.a-offscreen')[0].get_text().replace('€', '').replace(',', '.'))
+        return float(
+            soup.find(id='corePriceDisplay_desktop_feature_div').select('.a-offscreen')[0]
+            .get_text().replace('€', '').replace(',', '.'))
 
-            
     def show_offers(self):
         for l in self.links:
             response = requests.get(l, headers)
             soup = BeautifulSoup(response.content, 'html.parser')
-            #print(f"Amazon:\n\t\t{self.get_product(soup)}\n\t\t- Link: {l}\n\t\t- Price: {self.get_price(soup)}")
+            # print(f"Amazon:\n\t\t{self.get_product(soup)}\n\t\t- Link: {l}\n\t\t- Price: {self.get_price(soup)}")
             webhook.send(f"Amazon:\n\t\t{self.get_product(soup)}\n\t\t- Link: {l}\n\t\t- Price: {self.get_price(soup)}")
+
 
 if __name__ == '__main__':
     digitec = DigitecParser(websites['Digitec'])
@@ -73,7 +78,7 @@ if __name__ == '__main__':
     except AttributeError:
         print("Try again")
         digitec.show_offers()
-    
+
     amazon = AmazonParser(websites["Amazon"])
     try:
         amazon.show_offers()
